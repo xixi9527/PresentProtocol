8b92ca0b816b14b1d5056c8dbbc9dfd31bacfefb
xixi9527 <710921065@qq.COM>
2018-07-17 18:49:11 +0800

    Create UIViewController+JLPresent.h

---
+++ PresentProtocol/JLPresent/UIViewController+JLPresent.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+JLPresent.h
+//  PresentProtocol
+//
+//  Created by 喻佳珞 on 2018/7/17.
+//  Copyright © 2018年 喻佳珞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (JLPresent)
+- (void)jl_presentViewController:(UIViewController *)viewControllerToPresent;
+@end
